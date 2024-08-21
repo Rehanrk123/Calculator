b2676338f597fd892479e89d9abd209a21514c1d
Rehan Khan <141252890+Rehanrk123@users.noreply.github.com>
2024-08-22 02:58:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ arr.forEach(key=>{
     key.addEventListener("click",(e)=>{
        
         let a = e.target.innerHTML;
+        
         if((screen.innerHTML=="0"&&a=="*")||(screen.innerHTML=="0"&&a=="/")||(screen.innerHTML=="0"&&a=="+")||(screen.innerHTML=="0"&&a=="/")){
         }
         else if((screen.innerHTML=="-"&&a=="*")||(screen.innerHTML=="-"&&a=="/")||(screen.innerHTML=="-"&&a=="+")||(screen.innerHTML=="-"&&a=="/")||(screen.innerHTML=="-"&&a=="-")){   
         }
         else if(a=="="){
             let b = eval(string);
+            console.log(b)
             screen.innerHTML = b;
             string = b;
         }
@@ -29,6 +31,9 @@ arr.forEach(key=>{
                 string = screen.innerHTML.slice(0,screen.innerHTML.length-1);
                 screen.innerHTML = string; 
             }  
+        }
+        else if(string.length>12){
+
         }
         else if(a=="."&&string==""){
             screen.innerHTML = "0.";
